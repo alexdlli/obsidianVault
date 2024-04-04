021e019e87caae088367daead0a678afd02e43f6
alexdii <alexsandrojr.303@gmail.com>
2024-04-04 19:25:59 -0300

    vault backup: 2024-04-04 19:25:59

---
+++ teste/index.js
@@ -0,0 +1,13 @@
+var idadeAlex = 24
+var idadeYummi = 2
+
+function validarIdade(idade) {
+  if (idade >= 18) {
+    return "> ou = a 18"
+  } else {
+    return "< a 18"
+  }
+}
+
+validarIdade(idadeAlex)
+validarIdade(idadeYummi)
